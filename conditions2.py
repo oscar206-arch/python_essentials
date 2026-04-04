53cd95710bdb416c3328aa3b683b0bc8d6b01d96
Oscar Mwanjala <mwanjala001@gmail.com>
2026-04-04 15:03:12 +0300

    updated the new file with some code

---
+++ conditions2.py
@@ -0,0 +1,17 @@
+#Read three numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+largest_number = number1
+
+#Check if the second number is larger than the current largest_number and update the largest_number if needed.
+if number2 > largest_number:
+    largest_number = number2
+
+#Check if the third number is larger than the current largest_number and update the largest_number if needed.
+if number3 > largest_number:
+    largest_number = number3
+
+#Print the result
+print("The largest number is:", largest_number)
